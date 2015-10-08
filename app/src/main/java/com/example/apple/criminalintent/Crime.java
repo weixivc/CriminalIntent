2915d7b6bb1960e7985a00d8169feefe04283581
weixivc <weixi.vc@gmail.com>
2015-10-08 10:58:00 -0400

    Initial commit

---
+++ app/src/main/java/com/example/apple/criminalintent/Crime.java
@@ -0,0 +1,23 @@
+package com.example.apple.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by apple on 10/6/15.
+ */
+public class Crime {
+    private UUID mId;
+    private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
+
+    public Crime() { mId = UUID.randomUUID();  }
+    public UUID getId() { return mId;  }
+    public String getTitle() { return mTitle;}
+    public void setTitle(String title) { mTitle = title;}
+    public Date getDate() {return mDate;}
+    public void setDate(Date date) {mDate = date;}
+    public boolean isSolved() {  return mSolved;}
+    public void setSolved(boolean solved) { mSolved = solved;  }
+}
