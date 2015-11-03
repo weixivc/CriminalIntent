f976f84c13100de593dbd08ff3a80f738333cd04
weixivc <weixi.vc@gmail.com>
2015-11-03 11:25:06 -0500

    Chap 14

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeFragment.java
@@ -18,17 +18,15 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 
-import com.example.apple.criminalintent.Crime;
-import com.example.apple.criminalintent.CrimeLab;
 import com.example.apple.criminalintent.R;
 
 import java.util.Date;
 import java.util.UUID;
 
 /**
- * Created by Afei on 15-10-6.
+ * Created by anastasia on 04/10/2015.
  */
-public class CrimeFragment extends Fragment{
+public class CrimeFragment extends Fragment {
 
     private static final String ARG_CRIME_ID = "crime_id";
     private static final String DIALOG_DATE = "DialogDate";
@@ -36,12 +34,12 @@ public class CrimeFragment extends Fragment{
 
     private static final int REQUEST_DATE = 0;
     private static final int REQUEST_TIME = 1;
-
+    private static final String DELETE = "This crime is about to be deleted";
     private Crime mCrime;
     private EditText mTitleField;
     private Button mDateButton;
-    private Button mTimeButton;
     private CheckBox mSolvedCheckBox;
+    private Button mTimeButton;
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -53,84 +51,75 @@ public class CrimeFragment extends Fragment{
     }
 
     @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.fragment_crime, menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.menu_item_delete_crime:
-                CrimeLab.get(getActivity()).deleteCrime(mCrime);
-                getActivity().finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-
-    @Override
-    public void onCreate(Bundle savedIntanceState) {
-        super.onCreate(savedIntanceState);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         UUID crimeId = (UUID) getArguments().getSerializable(ARG_CRIME_ID);
         mCrime = CrimeLab.get(getActivity()).getCrime(crimeId);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        CrimeLab.get(getActivity())
+                .updateCrime(mCrime);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_crime, container,false);
+        View v = inflater.inflate(R.layout.fragment_crime, container, false);
+
         mTitleField = (EditText)v.findViewById(R.id.crime_title);
         mTitleField.setText(mCrime.getTitle());
         mTitleField.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                // Intentionally blank
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //This space intentionally left blank
             }
 
             @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                mCrime.setTitle(charSequence.toString());
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                mCrime.setTitle(s.toString());
             }
 
             @Override
-            public void afterTextChanged(Editable editable) {
-                // Intentionally blank
+            public void afterTextChanged(Editable s) {
+                //This one too
             }
         });
 
-        mDateButton = (Button)v.findViewById(R.id.crime_date);
-        updateDate();
+        mDateButton = (Button) v.findViewById(R.id.crime_date);
+        updateDate(android.text.format.DateFormat.format("EEEE, MMM dd, yyyy", mCrime.getDate()).toString());
         mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentManager manager = getActivity().getSupportFragmentManager();
+
+                FragmentManager manager = getFragmentManager();
                 DatePickerFragment dialog = DatePickerFragment.newInstance(mCrime.getDate());
                 dialog.setTargetFragment(CrimeFragment.this, REQUEST_DATE);
                 dialog.show(manager, DIALOG_DATE);
+
             }
         });
 
-        mTimeButton = (Button)v.findViewById(R.id.crime_time);
+        mTimeButton = (Button) v.findViewById(R.id.crime_time);
         updateTime();
         mTimeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FragmentManager manager = getActivity().getSupportFragmentManager();
-                com.bignerdranch.android.criminalintent.TimePickerFragment dialog = com.bignerdranch.android.criminalintent.TimePickerFragment.newInstance(mCrime.getDate());
+                FragmentManager manager = getFragmentManager();
+                TimePickerFragment dialog = TimePickerFragment.newInstance(mCrime.getDate());
                 dialog.setTargetFragment(CrimeFragment.this, REQUEST_TIME);
                 dialog.show(manager, DIALOG_TIME);
-
             }
         });
 
-        mSolvedCheckBox = (CheckBox)v.findViewById(R.id.crime_solved);
+        mSolvedCheckBox = (CheckBox) v.findViewById(R.id.crime_solved);
         mSolvedCheckBox.setChecked(mCrime.isSolved());
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                //set the crime's solved property
+                //Set the crime's solved property
                 mCrime.setSolved(isChecked);
             }
         });
@@ -139,31 +128,51 @@ public class CrimeFragment extends Fragment{
     }
 
     @Override
-    public void onActivityResult(int requrestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode != Activity.RESULT_OK) {
             return;
         }
 
-        if (requrestCode == REQUEST_DATE) {
+        if (requestCode == REQUEST_DATE) {
             Date date = (Date) data.getSerializableExtra(DatePickerFragment.EXTRA_DATE);
             mCrime.setDate(date);
-            updateDate();
+            updateDate(mCrime.getDate().toString());
         }
 
-        if (requrestCode == REQUEST_TIME) {
-            Date date = (Date) data.getSerializableExtra(com.bignerdranch.android.criminalintent.TimePickerFragment.EXTRA_TIME);
+        if (requestCode == REQUEST_TIME) {
+            Date date = (Date) data.getSerializableExtra(TimePickerFragment.EXTRA_TIME);
             mCrime.setDate(date);
             updateTime();
         }
-
     }
 
-    private void updateDate() {
-        mDateButton.setText(android.text.format.DateFormat.format("MMM dd, yyyy", mCrime.getDate()));
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_crime, menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_del_crime:
+                //Toast.makeText(getActivity(), DELETE, Toast.LENGTH_SHORT).show();
+                UUID id = mCrime.getId();
+                CrimeLab.get(getActivity()).deleteCrime(id);
+                getActivity().finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void updateDate(String text) {
+        mDateButton.setText(text);
     }
 
     private void updateTime() {
-        mTimeButton.setText(android.text.format.DateFormat.format("hh:mm", mCrime.getDate()));
+        mTimeButton.setText(android.text.format.DateFormat.format("hh:mm a", mCrime.getDate()));
     }
 
 }
